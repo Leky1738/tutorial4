47a51ce78ac2f087cd7982560a628aec19f1431c
Leky1738 <133865028+Leky1738@users.noreply.github.com>
2023-08-15 15:54:17 +1200

    Update CalcTest2.java

--- src/test/java/org/example/CalcTest2.java
+++ src/test/java/org/example/CalcTest2.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalcTest2 {
+public class CalcTest {
 
         Calc c = new Calc();
 
